d9aa03dbb430a582e8034cb43079d5a9a8501770
Gustavo Emanuel <gustavoe.dev@gmail.com>
2024-12-13 11:21:23 -0300

    feat: criação das views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+def view_products(request):
+    products = Product.objects.all()
+
+    context = {'products': products}
+
+    return render(request, 'products.html', context)
+
+def view_details_product(request, code):
+    product = Product.objects.get(code=code)
+
+    context = {'product': product}
+
+    return render(request, 'product_details.html', context)
